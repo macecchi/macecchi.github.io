6bf72d96a7e9a9c141efc4c83cae6ba2eb17d6e1
Mario Cecchi <macecchi@gmail.com>
2015-05-27 01:00:16 -0300

    New theme and content update

--- console.js
+++ console.js
@@ -2,35 +2,27 @@
 // http://meriw.com
 
 $(document).ready(function() {
-	init_text = "meriw$";
-	version = "0.2";
-	cursor = "&nbsp;";
-	default_theme = "homebrew";
+	init_text = '<span class="user">visitor</span>@<span class="host">meriw:</span><span class="path">/</span>$';
+	version = '0.3';
+	cursor = '&nbsp;';
+	default_theme = 'solarized-dark';
 	welcome_text = "Welcome to meriw (version " + version + ")\n\This is the personal website of Mario Cecchi.\nEnter 'help' to view the menu.\n ";
 	
 	available_commands = new Array(
-		["about", "About me", function(){ stdout("Mario Cecchi.\n\
-22-year-old Computer Science student at the Federal University of Rio de Janeiro (UFRJ), Brazil.\n\
-I also work as a researcher at the Computational Intelligence Laboratory, UFRJ.\n\
+		["about", "About me", function(){ stdout("Mario Cecchi.\n\n\
+22-year-old Computer Science student at Federal University of Rio de Janeiro (UFRJ), Brazil.\n\
+I also work as an undergraduate researcher at the Computational Intelligence Laboratory, UFRJ.\n\
 In my spare time, I code and manage a few websites and some personal projects.\n\
-My biggest interest is programming, either in C, C++, Objective-C, Java, PHP, JavaScript or MATLAB.\n\
+My biggest interest is in mobile and web development.\n\
 \n\
-Please find more about what I do through the 'projects' menu.\n\n") }],
+Please find more about what I do through the 'projects' menu.\n\n\
+The code for this Terminal-style page is available on my GitHub at https://github.com/macecchi/macecchi.github.io.\n") }],
 		["email", "My email", function(){ stdout("Feel free to contact me at macecchi@gmail.com.") }],
 		["projects", "Projects and research", function() { stdout("\
-My projects at the moment are the following:\n\
-	LabicKinect: 3D environment mapping using Kinect\n\
-	Home automation with Raspberry Pi\n\
-\n\
-I'm also part of the team of the following websites, where I do all or most of the coding:\n\
-	Katy Perry Brasil: http://www.katyperry.com.br/\n\
-	Hilary Duff Brasil: http://www.hilaryduff.com.br/\n\
-	Portal Focka: http://www.portalfocka.com.br/\n\
-\n\
-My research interests are computer vision, computational intelligence and software development.\n\
-\n\
-You can find more about my projects on my Lattes CV or on my GitHub page.\n\n")}],
+Please refer to my GitHub page for information about my projects.\n\
+https://github.com/macecchi (or run 'github')\n")}],
 		["github", "My GitHub account", function() { redirect("https://github.com/macecchi"); }],
+		["linkedin", "My Linkedin profile", function() { redirect("https://br.linkedin.com/pub/mario-cecchi-raduan/71/757/124"); }],
 		["lattes", "My Lattes CV", function() { redirect("http://lattes.cnpq.br/2694166859171258"); }],
 		["facebook", "My Facebook profile", function() { redirect("https://www.facebook.com/macecchi"); }],
 		["twitter", "My Twitter profile", function() { redirect("https://twitter.com/MarioCecchi"); }],
@@ -47,7 +39,7 @@ You can find more about my projects on my Lattes CV or on my GitHub page.\n\n")}
 							stdout("Invalid theme option.");
 					}
 				} else {
-					stdout("usage: theme <THEME_NAME>\nAvailable themes:\n homebrew (default)\n basic");
+					stdout("usage: theme <THEME_NAME>\nAvailable themes:\n solarized-dark (default)\n homebrew\n basic");
 				}
 		}],
 		["clear", "Clear terminal screen", function() { target.empty(); }],
@@ -109,12 +101,8 @@ You can find more about my projects on my Lattes CV or on my GitHub page.\n\n")}
 	});
 	
 	function update_line() {
-		//if (!isMobile.any()) {
-			target_line.html(init_text + " " + command + "<span id=\"cursor\">" + cursor + "</span>");
-		//} else {
-			//target_line.html(init_text + " <input type=\"text\" id=\"fakeinput\" autocapitalize=\"off\" autocorrect=\"off\" placeholder=\"tap here to type\">");
-			//target_line.children().focus();
-		//}
+		target_line.html(init_text + " " + command + "<span id=\"cursor\">" + cursor + "</span>");
+
 	}
 	
 	function new_line() {
@@ -127,7 +115,6 @@ You can find more about my projects on my Lattes CV or on my GitHub page.\n\n")}
 	}
 
 	function eval_command() {
-		//console.log("Command: '" + command.valueOf() + "'");
 		argv = command.split(" ");
 		argc = argv.length;
 		
@@ -173,10 +160,7 @@ You can find more about my projects on my Lattes CV or on my GitHub page.\n\n")}
 		for (i=0; i<available_commands.length; i++) {
 			cmd = available_commands[i];
 			stdout("	" + cmd[0] + " - " + cmd[1] + "\n");
-		}
-		stdout("\n");
-		//stdout("Total: " + available_commands.length + "");
-	}
+		}	}
 	
 	function redirect(url) {
 		if (url == "") {
@@ -185,9 +169,7 @@ You can find more about my projects on my Lattes CV or on my GitHub page.\n\n")}
 		} else {
 			stdout("=> Redirecting to " + url + ". Loading...");
 			self.location.href = url;
-		}
-		// setInterval(function() { target.append("."); }, 100);
-	}
+		}	}
 	
 	function exit() {
 		stdout("Goodbye!");
@@ -205,8 +187,6 @@ You can find more about my projects on my Lattes CV or on my GitHub page.\n\n")}
 		$("head").append("<meta content='width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0' name='viewport'>\n<meta content='yes' name='apple-mobile-web-app-capable'>");
 		stdout("<span class=\"welcome\">\nPlease use your computer to access this website.\n</span>");
 		
-		//$('#fakeinput').focus().blur().focus();
-
 	}
 
 	new_line();
